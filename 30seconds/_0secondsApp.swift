8aaf958cfcaff9088d260a7ae88b7dee031cc851
Max Peerutin <peerutinm@gmail.com>
2023-06-26 15:28:25 +0200

    Initial Commit

---
+++ 30seconds/_0secondsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0secondsApp.swift
+//  30seconds
+//
+//  Created by Max Peerutin on 2023/06/26.
+//
+
+import SwiftUI
+
+@main
+struct _0secondsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
